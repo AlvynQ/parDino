8d025f114eed3ba9b6bc3371c9c61c8b81a36fc5
AlvynQ <66834397+AlvynQ@users.noreply.github.com>
2020-09-07 16:47:24 +0200

    js

--- app.js
+++ app.js
@@ -1,68 +1,32 @@
-// let dinoN = ["T-rex le Carnasier",
-//             "Le Commandant Brachiosaure",
-//             "Le Fantassin Ankylosaure",
-//             "L'assasin Spinosaure",
-//             "Le Microraptor Psychopompe",
-//             "La Pierre d'Extinction"],
 
-//     dinoV = [3,
-//               2,
-//               1,
-//               spinosaure,
-//               microraptor,
-//               -1];
+class Dice {
+    constructor(name, img, value, contrains) {
+      this.name = name;
+      this.img = img;
+      this.value = value;
+      this.contrains = contrains;
+    }
+  }
 
-const faceDice = [{
-    name : "T-rex le Carnasier";
-    img : "vicking.png",
-    value : 3,
-    contrainte : null
-},
-    {
-    img : "assasin.png",
-    value : 1,
-    contrainte : "nullVicking"
-},
-    {
-    img : "mage.png",
-    value : null,
-    contrainte : "nullVicking"
-},
-{
-    img : "bouclier.png",
-    value : 10,
-   contrainte : "nullVicking"
-},
-{
-    img : "épée.png",
-    value : 8,
-    contrainte : "nullVicking"
-},
-{
-    img : "bague.png",
-    value : 2,
-    contrainte : "nullVicking"
-},
 
-]
+const dice1 = new Dice("T-rex le Carnasier", "tRex.png", 3, null);
+const dice2 = new Dice("Le Commandant Brachiosaure", "brachiosaure.png", 2, null);
+const dice3 = new Dice("Le Fantassin Ankylosaure", "ankylosaure.png", 1, null);
+const dice4 = new Dice("L'assasin Spinosaure", "spinosaure.png", 1, null);
+const dice5 = new Dice("Le Microraptor Psychopompe", "microraptor.png", 0, null);
+const dice6 = new Dice("La Pierre d'Extinction", "pierre.png", -1, null);
+
+
+const diceFaceArray = [dice1, dice2, dice3, dice4, dice5, dice6];
+
 
 let nbDice = 7; 
 
-let maxRandom = faceDice.length;
+let maxRandom = diceFaceArray.length;
 
 //let fullDiceArray = [];
 // let tabfullDice = fullDiceArray.push(random);
     
-
-
-let spinosaure = 0;
-let microraptor = 0;
-let ankylosaure = 1;
-let tRex = 3;
-let brachiosaure = 2;
-let pierreExt = -1;
-
-
 const dice = document.querySelectorAll(".dice");
 
 function randomDice(){
@@ -71,7 +35,7 @@ function randomDice(){
     for (; i < nbDice ; i++) {
     const random = Math.floor(Math.random() * maxRandom);
     
-    dice[i].innerHTML = `<data-value="${faceDice[random].value}">${faceDice[random].value}  <img src="./img/${faceDice[random].img}">`;
+    dice[i].innerHTML = `<data-value="${diceFaceArray[random].value}">${diceFaceArray[random].value}  <img src="./img/${diceFaceArray[random].img}">`;
     console.log(i);
     }
     play();
